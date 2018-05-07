f34df5b1f99fb976e508c0909b23dbbea294900e
Nao Takegawa <nao.takegawa316@gmail.com>
2018-05-07 21:23:48 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PropTypes from 'prop-types';
+import Timeline from 'react-calendar-timeline/lib';
+import moment from 'moment';
+
+class App extends Component {
+
+  render() {
+    const test = "abcdef";
+    const groups = [
+      {id: 1, title: 'group 1'},
+      {id: 2, title: 'group 2'}
+    ]
+
+    const items = [
+      {id: 1, group: 1, title: 'item 1', start_time: moment(), end_time: moment().add(1, 'hour')},
+      {id: 2, group: 2, title: 'item 2', start_time: moment().add(-0.5, 'hour'), end_time: moment().add(0.5, 'hour')},
+      {id: 3, group: 1, title: 'item 3', start_time: moment().add(2, 'hour'), end_time: moment().add(3, 'hour')}
+    ]
+
+    return (
+      <div className="App">
+        <Timeline groups={groups}
+              items={items}
+              defaultTimeStart={moment().add(-12, 'hour')}
+              defaultTimeEnd={moment().add(12, 'hour')}
+              />
+        <span>{test}</span>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/generate-fake-data.js
@@ -0,0 +1,6 @@
+import faker from 'faker'
+import randomColor from 'randomcolor'
+import moment from 'moment'
+
+export default function (groupCount = 30, itemCount = 1000, daysInPast = 30) {
+}
